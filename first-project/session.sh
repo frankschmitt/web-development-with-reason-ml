9af1cc721e7252569610f0a6093cacfa01e3c25b
Frank Schmitt <frankschmitt@gmx.de>
2021-05-18 21:57:43 +0200

    use commonjs instead of es6 (fixes 'unexpected token export')

---
+++ first-project/session.sh
@@ -0,0 +1,17 @@
+export BASEDIR=~/src/web-development-with-reason-ml/first-project
+export SRCDIR=$BASEDIR/src
+# vim session with source files
+tmux "new-session" -s "dev" -d
+tmux send-keys -t dev 'cd $SRCDIR' C-m
+tmux send-keys -t dev 'vim *.re' C-m
+# npm for automatic recompilation
+tmux "split-window" -h -p 30
+tmux send-keys -t dev 'cd $BASEDIR' C-m
+tmux send-keys -t dev 'npm run start' C-m
+# interactive console, for running scripts with node
+tmux "split-window" -v
+tmux send-keys -t dev 'cd $SRCDIR' C-m
+tmux send-keys -t dev 'node *.bs.js' C-m
+# attach to session
+tmux attach -t "dev"
+
