454d6887eb7a07248879e6def86b13ad12c362b5
Frank Schmitt <frankschmitt@gmx.de>
2021-05-29 22:11:12 +0200

    adapt session.sh to new file extension .res

--- first-project/session.sh
+++ first-project/session.sh
@@ -3,7 +3,7 @@ export SRCDIR=$BASEDIR/src
 # vim session with source files
 tmux "new-session" -s "dev" -d
 tmux send-keys -t dev 'cd $SRCDIR' C-m
-tmux send-keys -t dev 'vim *.re' C-m
+tmux send-keys -t dev 'vim *.res' C-m
 # npm for automatic recompilation
 tmux "split-window" -h -p 30
 tmux send-keys -t dev 'cd $BASEDIR' C-m
